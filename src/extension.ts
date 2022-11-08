7495379c7f3235865ffea97e7db08e8423122ccc
Jan0660 <jan0660@tutanota.com>
2022-11-08 16:17:05 +0100

    feat: allow language server path to be an executable

--- src/extension.ts
+++ src/extension.ts
@@ -23,36 +23,23 @@ import { createClientPipeTransport } from "vscode-jsonrpc/node";
 import { createConnection } from "net";
 
 export function activate(context: ExtensionContext) {
-    // return;
-    // The server is implemented in node
-    let serverExe = "dotnet";
-
-    // let serverExe = "D:\\Development\\Omnisharp\\csharp-language-server-protocol\\sample\\SampleServer\\bin\\Debug\\netcoreapp2.0\\win7-x64\\SampleServer.exe";
-    // let serverExe = "D:/Development/Omnisharp/omnisharp-roslyn/artifacts/publish/OmniSharp.Stdio.Driver/win7-x64/OmniSharp.exe";
-    // The debug options for the server
-    // let debugOptions = { execArgv: ['-lsp', '-d' };5
-
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
-    // let dll = "C:/Users/Jan/RiderProjects/RCaron/RCaron.LanguageServer/bin/Release/net7.0/win-x64/RCaron.LanguageServer.dll";
-    let dll = workspace.getConfiguration('rcaron', workspace.workspaceFolders[0].uri).get<string>('languageServerPath');
-    if (dll == null)
+    let serverPath = workspace.getConfiguration('rcaron', workspace.workspaceFolders[0].uri).get<string>('languageServerPath');
+    if (serverPath == null)
         return;
-    let serverOptions: ServerOptions = {
-        // run: { command: serverExe, args: ['-lsp', '-d'] },
+    let serverOptions: ServerOptions = serverPath.endsWith('.dll') ? {
         run: {
-            command: serverExe,
-            args: [dll],
+            command: "dotnet",
+            args: [serverPath],
             transport: TransportKind.pipe,
         },
-        // debug: { command: serverExe, args: ['-lsp', '-d'] }
         debug: {
-            command: serverExe,
-            args: [dll],
+            command: "dotnet",
+            args: [serverPath],
             transport: TransportKind.pipe,
-            runtime: "",
         },
-    };
+    } : { run: { command: serverPath, transport: TransportKind.pipe }, debug: { command: serverPath, transport: TransportKind.pipe } };
     // let time = 100;
     // let serverOptions = async () => {
     //     await new Promise((r) => setTimeout(r, time));
